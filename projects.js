b47906c06f66469bf9e2ac134bf678e4d7cf0ee6
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-08-16 20:15:48 -0400

    Endpoints * All Project endpoints complete * More validation middleware added

--- projects.js
+++ projects.js
@@ -33,15 +33,49 @@ router.post('/', validateProject, async (req, res, next) => {
 })
 
 //PUT
+router.put('/:id', validateProjectId, async (req, res, next) => {
+  try {
+    const project = await projectDb.update(req.params.id, req.body);
+    res.status(201).json({message: "Project has been updated!", project });
+  } catch (err) {
+    next(err);
+  }
+});
 
 //DELETE
+router.delete('/:id', validateProject, async (req, res, next) => {
+  try {
+    const deletedProject = await projectDb.get(req.params.id);
+    const expulsion = await projectDb.delete(req.params.id);
+    res.status(200).json({message: "This project has been removed: ", deletedProject})
+  } catch (err) {
+    next(err)
+  }
+});
 
 // Middleware
 function validateProject(req, res, next) {
-  if (!req.body.description || !req.body.notes) {
+  if (!req.body.description || !req.body.name) {
     res.status(400).json({ message: "Missing description or notes" })
   }
   return next();
+}
+
+function validateProjectId(req, res, next) {
+  projectDb.get(req.params.id)
+    .then((project) => {
+      if (project) {
+        req.project = project;
+        next();
+      }
+      return res.status(400).json({ message: 'Invalid project id' });
+    })
+    .catch((error) => {
+			console.log(error);
+			return res.status(500).json({
+				error: 'There was a problem pulling the data from the server',
+      })
+    });
 }
 
 module.exports = router;
